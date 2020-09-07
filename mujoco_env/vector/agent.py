be372f1f07ff0a724d6d554957b1be8d866f48a9
Christian Leininger <christianleininger42@gmail.com>
2020-09-07 20:40:13 +0200

    Add files via upload

--- mujoco_env/vector/agent.py
+++ mujoco_env/vector/agent.py
@@ -1,157 +1,123 @@
-import copy
-import numpy as np
 import torch
+from models import Actor, Critic
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 
-# Implementation of Twin Delayed Deep Deterministic Policy Gradients (TD3)
-# Paper: https://arxiv.org/abs/1802.09477
-
-
-class Actor(nn.Module):
-    def __init__(self, state_dim, action_dim, max_action):
-        super(Actor, self).__init__()
-        self.l1 = nn.Linear(state_dim, 256)
-        self.l2 = nn.Linear(256, 256)
-        self.l3 = nn.Linear(256, action_dim)
-        self.max_action = max_action
-    
-    def forward(self, state):
-        a = F.relu(self.l1(state))
-        a = F.relu(self.l2(a))
-        return self.max_action * torch.tanh(self.l3(a))
-
-
-class Critic(nn.Module):
-    def __init__(self, state_dim, action_dim):
-        super(Critic, self).__init__()
-        # Q1 architecture
-        self.l1 = nn.Linear(state_dim + action_dim, 256)
-        self.l2 = nn.Linear(256, 256)
-        self.l3 = nn.Linear(256, 1)
-        
-        # Q2 architecture
-        self.l4 = nn.Linear(state_dim + action_dim, 256)
-        self.l5 = nn.Linear(256, 256)
-        self.l6 = nn.Linear(256, 1)
-        
-    def forward(self, state, action):
-        sa = torch.cat([state, action], 1)
-        
-        q1 = F.relu(self.l1(sa))
-        q1 = F.relu(self.l2(q1))
-        q1 = self.l3(q1)
-        
-        q2 = F.relu(self.l4(sa))
-        q2 = F.relu(self.l5(q2))
-        q2 = self.l6(q2)
-        return q1, q2
-    
-    def Q1(self, state, action):
-        sa = torch.cat([state, action], 1)
-        q1 = F.relu(self.l1(sa))
-        q1 = F.relu(self.l2(q1))
-        q1 = self.l3(q1)
-        return q1
-
-
 class TD3(object):
+    """ TD3 plus the Ensemble of Critics as an agent object
+    to act and update the networkweights, save and laod the weights
+    """
     def __init__(self, state_dim, action_dim, max_action, args):
         self.actor = Actor(state_dim, action_dim, max_action).to(args.device)
-        self.actor_target = copy.deepcopy(self.actor)
-        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=3e-4)
-        
+        self.actor_target = Actor(state_dim, action_dim, max_action).to(args.device)
+        self.actor_target.load_state_dict(self.actor.state_dict())
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters())
         self.critic = Critic(state_dim, action_dim).to(args.device)
-        self.critic_target = copy.deepcopy(self.critic)
-        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=3e-4)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters())
+        self.target_critic = Critic(state_dim, action_dim).to(args.device)
+        self.target_critic.load_state_dict(self.target_critic.state_dict())
         self.max_action = max_action
-        self.num_q_target = args.num_q_target
-        self.update_counter = 0
-        self.step = 0 
-        self.total_it = 0 
         self.batch_size = args.batch_size
         self.discount = args.discount
-        self.tau = args.tau
+        self.tau = args.tau 
         self.policy_noise = args.policy_noise
         self.noise_clip = args.noise_clip
         self.policy_freq = args.policy_freq
         self.device = args.device
-
+        self.step = 0 
+    
     def select_action(self, state):
-        state = torch.FloatTensor(state.reshape(1, -1)).to(self.device)
+        state = torch.Tensor(state.reshape(1, -1)).to(self.device)
         return self.actor(state).cpu().data.numpy().flatten()
     
     
-    def train(self, replay_buffer, writer, iterations=1):
-
-        self.total_it += 1
-        for it  in range(iterations):
-            # Sample replay buffer 
-            state, next_state,  action, reward, not_done = replay_buffer.sample(self.batch_size)
-            state = torch.Tensor(state).to(self.device)
-            next_state = torch.Tensor(next_state).to(self.device)
-            action = torch.Tensor(action).to(self.device)
-            reward = torch.Tensor(reward).to(self.device)
-            not_done = torch.Tensor(not_done).to(self.device)
-
-            #with torch.no_grad():
-            # Select action according to policy and add clipped noise
-            with torch.no_grad():
-                noise = (torch.randn_like(action) * self.policy_noise).clamp(-self.noise_clip, self.noise_clip)
-                next_action = (self.actor_target(next_state) + noise).clamp(-self.max_action, self.max_action)
+    def train(self, replay_buffer, writer, iterations):
+        """ Update function for the networkweights of the Actor and Critis 
+            current and Target by useing the 3 new features of the TD3 paper
+            to the DDPG implementation
+            1. Delay the policy Updates 
+            2. Two crtitc networks take the min Q value
+            3. Target Policy Smoothing
+            Own use an Ensemble Approach of delayed updated critics 
+        
+        """
+        self.step += 1
+        
+        for it in range(iterations):
             
-                # Compute the target Q value
-                target_Q1, target_Q2 = self.critic_target(next_state, next_action)
-                target_Q = torch.min(target_Q1, target_Q2)
-                target_Q = reward + ((not_done -1) * self.discount * target_Q)
-            # Get current Q estimates
-            current_Q1, current_Q2 = self.critic(state, action)
+            # Step 1: Sample a batch of transitions (s, s’, a, r) from the memory
+            batch_states, batch_next_states, batch_actions, batch_rewards, batch_dones = replay_buffer.sample(self.batch_size)
+            # convert the numpy arrays to tensors object 
+            # if cuda is available send data to gpu
+            state = torch.Tensor(batch_states).to(self.device)
+            next_state = torch.Tensor(batch_next_states).to(self.device)
+            action = torch.Tensor(batch_actions).to(self.device)
+            reward = torch.Tensor(batch_rewards).to(self.device)
+            done = torch.Tensor(batch_dones).to(self.device)
             
-            # Compute critic loss
+            # Step 2: use the  Target Actor  to create the action of the next
+            # state (part of the TD-Target 
+            next_action = self.actor_target(next_state)
+            
+            # Step 3: Add Gaussian noise to the action for exploration 
+            # clip the action value in case its outside the boundaries 
+            noise = torch.Tensor(batch_actions).data.normal_(0, self.policy_noise).to(self.device)
+            noise = noise.clamp(-self.noise_clip, self.noise_clip)
+            next_action = (next_action + noise).clamp(-self.max_action, self.max_action)
+            
+            # Step 4: Use the differenet Target Critis (delayed update)
+            # and min of two  Critic from TD3 to create the different Q Targets
+            # compute the average of all Q Targets to get a single value
+            target_Q1, target_Q2 = critic(next_state, next_action) 
+            target_Q = torch.min(target_Q1, target_Q2)
+            
+           
+           # Step 5: Create the update based on the bellman  equation 
+            target_Q = reward + ((1 - done) * self.discount * target_Q).detach()
+            
+            # Step 6: Use the critic compute the Q estimate for current state and action
+            current_Q1, current_Q2 = self.critic(state, action) 
+            
+            # Step 7: Compute the critc loss with the mean squard error
+            # loss function 
             critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+            writer.add_scalar('critic_loss', critic_loss , self.step)
             
-            # Optimize the critic
+
+            # Step 8: Backpropagate this Critic loss and update the parameters
+            # of the two Critic models use the adam optimizer
+            
             self.critic_optimizer.zero_grad()
             critic_loss.backward()
             self.critic_optimizer.step()
-            # Delayed policy updates
-            if self.total_it % self.policy_freq == 0:
-                # Compute actor losse
-                actor_loss = -self.critic.Q1(state, self.actor(state)).mean()
             
-                # Optimize the actor 
+            # Step 9: Delayed update og Actor model
+            if it % self.policy_freq == 0:
+                actor_loss = -self.critic.Q1(state, self.actor(state)).mean()
+                writer.add_scalar('actor_loss', actor_loss , self.step)
                 self.actor_optimizer.zero_grad()
                 actor_loss.backward()
                 self.actor_optimizer.step()
                 
-                # Update the frozen target models
-                for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
-                    target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
-            
+                # Step 10: we update the weights of the Critic target by polyak averaging
+                # hyperparameter tau determines the combination of current and
+                # target weights
                 for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
                     target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+                
+                for param, target_param in zip(self.critic.parameters(), self.target_critic.parameters()):
+                    target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+     
+                
     
-    
-    def hardupdate(self):
-        pass
-
-    def save(self, filename , directory):
+    # Making a save method to save a trained model
+    def save(self, filename, directory):
         torch.save(self.actor.state_dict(), '%s/%s_actor.pth' % (directory, filename))
         torch.save(self.critic.state_dict(), '%s/%s_critic.pth' % (directory, filename))
-        
-        #torch.save(self.critic.state_dict(), filename + "_critic")
-        #torch.save(self.critic_optimizer.state_dict(), filename + "_critic_optimizer")
-		
-        #torch.save(self.actor.state_dict(), filename + "_actor")
-        #torch.save(self.actor_optimizer.state_dict(), filename + "_actor_optimizer")
     
-    def load(self, filename):
-        self.critic.load_state_dict(torch.load(filename + "_critic"))
-        self.critic_optimizer.load_state_dict(torch.load(filename + "_critic_optimizer"))
-        self.critic_target = copy.deepcopy(self.critic)
-        
-        self.actor.load_state_dict(torch.load(filename + "_actor"))
-        self.actor_optimizer.load_state_dict(torch.load(filename + "_actor_optimizer"))
-        self.actor_target = copy.deepcopy(self.actor)		
+    # Making a load method to load a pre-trained model
+    def load(self, filename, directory):
+        self.actor.load_state_dict(torch.load('%s/%s_actor.pth' % (directory, filename)))
+        self.critic.load_state_dict(torch.load('%s/%s_critic.pth' % (directory, filename)))
